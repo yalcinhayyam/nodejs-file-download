4fd9a48c70a5668ea4d292083b7f01bfff328e38
Hayyam Nazım Yalçın <hayyam.yalcin@veripark.com>
2023-12-10 00:54:59 +0300

    ok

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(`<iframe src='https://view.officeapps.live.com/op/embed.aspx?src=http://remote.url.tld/path/to/document.doc' width='1366px' height='623px' frameborder='0'>This is an embedded <a target='_blank' href='http://office.com'>Microsoft Office</a> document, powered by <a target='_blank' href='http://office.com/webapps'>Office Online</a>.</iframe>`)
+  res.send(`<iframe src='https://view.officeapps.live.com/op/embed.aspx?src=http://localhost:3000/file' width='1366px' height='623px' frameborder='0'>This is an embedded <a target='_blank' href='http://office.com'>Microsoft Office</a> document, powered by <a target='_blank' href='http://office.com/webapps'>Office Online</a>.</iframe>`)
 });
 app.get("/file", (req, res) => {
   const filePath = path.join(process.cwd(), "rapor.docx");
