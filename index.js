61bde396f1abf5af96c28f4b05fd610571e3c42f
H. Nazım Yalçın <50745494+yalcinhayyam@users.noreply.github.com>
2023-12-09 22:33:02 +0000

    ok

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(`<iframe src='https://view.officeapps.live.com/op/embed.aspx?src=http://localhost:3000/file' width='1366px' height='623px' frameborder='0'>This is an embedded <a target='_blank' href='http://office.com'>Microsoft Office</a> document, powered by <a target='_blank' href='http://office.com/webapps'>Office Online</a>.</iframe>`)
+  res.send(`
+  <iframe src="https://docs.google.com/gview?url=https://automatic-goggles-xv7rg9v74x92p5rr-4000.app.github.dev/file&embedded=true"></iframe>
+  <iframe 
+  src='https://view.officeapps.live.com/op/embed.aspx?src=https://automatic-goggles-xv7rg9v74x92p5rr-4000.app.github.dev/file' 
+  width='1366px' height='623px' frameborder='0'>This is an embedded <a target='_blank' href='http://office.com'>Microsoft Office</a> document, powered by <a target='_blank' href='http://office.com/webapps'>Office Online</a>.</iframe>`)
 });
 app.get("/file", (req, res) => {
+  res.setHeader("Content-disposition", "attachment; filename=rapor.docx");
+  // res.setHeader("Content-type", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+
   const filePath = path.join(process.cwd(), "rapor.docx");
   const stream = fs.createReadStream(filePath);
 
